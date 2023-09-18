3dbfb2d081bff9e98c2af19354b9c0c2f37436d9
Jonathan S Berry <jonathan.s.berry@gmail.com>
2023-09-18 10:43:47 +0100

    WRC-12 Improve test name

--- ckanext/who_romania/tests/test_helpers.py
+++ ckanext/who_romania/tests/test_helpers.py
@@ -9,6 +9,6 @@ class TestGetDates():
         (('2023-03', 1), ['07 Mar 2023', '14 Mar 2023', '21 Mar 2023', '28 Mar 2023']),
         (('2023-12', 0, '%Y-%m-%d'), ['2023-12-04', '2023-12-11', '2023-12-18', '2023-12-25']),
     ])
-    def test_get_dates(self, inputs, output):
+    def test_get_dates_of_weekdays_in_month(self, inputs, output):
         dates = who_romania_helpers.get_dates_of_weekday_in_month(*inputs)
         assert dates == output
