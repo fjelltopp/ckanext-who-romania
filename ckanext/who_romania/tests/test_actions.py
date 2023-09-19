452c9a9a21e146e40494cddb9853af2940ed79e4
Jonathan S Berry <jonathan.s.berry@gmail.com>
2023-09-19 17:34:49 +0100

    WRC-17 Test user_show_me action

--- ckanext/who_romania/tests/test_actions.py
+++ ckanext/who_romania/tests/test_actions.py
@@ -1,6 +1,8 @@
 import pytest
 import ckan.tests.factories as factories
 from ckan.tests.helpers import call_action
+import ckan.plugins.toolkit as toolkit
+from ckan import model
 
 
 @pytest.mark.ckan_config('ckan.plugins', "who_romania")
@@ -34,3 +36,18 @@ class TestListUsers():
         )
         user_ids_found = [u['id'] for u in response]
         assert user_ids_found == [users[2]['id']]
+
+
+@pytest.mark.ckan_config('ckan.plugins', 'who_romania')
+@pytest.mark.usefixtures('clean_db', 'with_plugins')
+class TestUserShowMe(object):
+
+    def test_no_user(self):
+        with pytest.raises(toolkit.NotAuthorized):
+            call_action('user_show_me', {})
+
+    def test_user(self):
+        user = factories.User()
+        user_obj = model.User.get(user['name'])
+        response = call_action('user_show_me', {'auth_user_obj': user_obj})
+        assert response['name'] == user['name']
