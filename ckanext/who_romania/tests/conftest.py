af58a12c3028089e8f7561d00c4f3ecf871ffa85
Jonathan S Berry <jonathan.s.berry@gmail.com>
2023-09-27 17:29:33 +0100

    WRC-21 Tests for automation actions

---
+++ ckanext/who_romania/tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+@pytest.fixture(scope='package')
+def vcr_config():
+    return {
+        "filter_post_data_parameters": [
+            ('assertion', 'DUMMY')
+        ],
+        "filter_headers": [
+            ('authorization', 'DUMMY'),
+            ('Authorization', 'DUMMY')
+        ],
+        "ignore_hosts": [
+            "127.0.0.1",
+            "0.0.0.0",
+            "192.168.49.2"
+        ]
+    }
